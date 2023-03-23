3bd7e9da27fdc872394dbe9f3148ee89a75baaa2
Vadim Gamzaev <Vadosbox@gmail.com>
2023-03-23 19:50:56 +0700

    - рефакторинг TestManagerTests - заменил OrderedTaskManagerTests на обновленные от Кирилл К. - добавил Stub для тестирования в отдельную папку Test Double

--- Framework/TaskManagerPackage/Tests/TaskManagerPackageTests/OrderedTaskManagerTests.swift
+++ Framework/TaskManagerPackage/Tests/TaskManagerPackageTests/OrderedTaskManagerTests.swift
@@ -9,138 +9,123 @@ import XCTest
 @testable import TaskManagerPackage
 
 final class OrderedTaskManagerTests: XCTestCase {
-	
-	var sut: ITaskManager!
-	var taskManager: ITaskManager!
-	
-	enum TaskStub {
-		static var tasks = [
-				RegularTask(
-					title: "SomeRegularTask1",
-					isComplete: false
-				),
-				RegularTask(
-					title: "SomeRegularTask2",
-					isComplete: false
-				),
-				RegularTask(
-					title: "SomeRegularTask3",
-					isComplete: true
-				),
-				RegularTask(
-					title: "SomeRegularTask4",
-					isComplete: true
-				),
-				ImportantTask(
-					title: "SomeImportantTask1",
-					taskPriority: .high,
-					createDate: Date()
-				),
-				ImportantTask(
-					title: "SomeImportantTask2",
-					taskPriority: .high,
-					createDate: Date()
-				)
-			]
-		}
-	
-	override func setUp() {
-		super.setUp()
-		taskManager = TaskManager()
-		sut = OrderedTaskManager(taskManager: taskManager)
-	}
-	
-	override func tearDown() {
-		sut = nil
-		taskManager = nil
-		super.tearDown()
-	}
-	
+
 	// MARK: AllTasks
 	func test_allTasks_withTasksFromTaskStub_shouldBeNotEmpty() {
+		let sut = makeSut()
 		sut.addTasks(tasks: TaskStub.tasks)
-		XCTAssertFalse(sut.allTasks().isEmpty, "Tasks must be not empty")
+		XCTAssertFalse(sut.allTasks().isEmpty, "sut.allTasks не содержит добавленных задач")
 	}
 	
 	func test_allTasks_initialState_shouldBeEmpty() {
-		XCTAssertTrue(sut.allTasks().isEmpty, "Tasks must be empty")
+		let sut = makeSut()
+		XCTAssertTrue(sut.allTasks().isEmpty, "sut.allTasks не должен содержать ни каких задач")
 	}
 	
 	// MARK: UnCompletedTasks
 	func test_unCompletedTasks_withTaskStub_shouldBeEmpty()  {
+		let sut = makeSut()
 		sut.addTasks(tasks: TaskStub.tasks)
 		sut.allTasks().forEach { $0.isComplete = true }
-		XCTAssertTrue(sut.uncompletedTasks().isEmpty, "Uncomleted tasks should be empty")
+		XCTAssertTrue(sut.uncompletedTasks().isEmpty, "sut.uncompletedTasks не должен содержать задачи")
 	}
 	
 	func test_unCompletedTasks_withNewTask_shouldBeNotEmpty() {
+		let sut = makeSut()
 		let task = RegularTask(title: "do test", isComplete: false)
 		sut.addTasks(tasks: [task])
-		XCTAssertFalse(sut.uncompletedTasks().isEmpty, "Uncompleted task should be not empty")
+		XCTAssertFalse(sut.uncompletedTasks().isEmpty, "sut.uncompletedTasks должен содержать задачи")
 	}
 	
-	func test_unCompletedTasks_withTaskStub_shouldBeNotEmpty() {
-		sut.addTasks(tasks: TaskStub.tasks)
-		XCTAssertFalse(!sut.uncompletedTasks().isEmpty, "Uncompleted tasks must be not empty")
+	func test_unCompletedTasks_withTask_shouldBeNotEmpty() {
+		let sut = makeSut()
+		sut.addTasks(tasks: [RegularTask(title: "do something")])
+		XCTAssertFalse(sut.uncompletedTasks().isEmpty, "sut.uncompletedTasks не должен быть пустым")
 	}
 	
 	// MARK: CompletedTasks
 	func test_completedTasks_withTaskRepository_shouldBeEmpty()  {
+		let sut = makeSut()
 		sut.addTasks(tasks: TaskStub.tasks)
 		sut.allTasks().forEach { $0.isComplete = false }
-		XCTAssertTrue(sut.completedTasks().isEmpty, "Completed tasks should be empty")
+		XCTAssertTrue(sut.completedTasks().isEmpty, "sut.completedTasks не должен содержать задачи")
 	}
 	
 	func test_completedTasks_withNewTask_shouldBeNotEmpty() {
+		let sut = makeSut()
 		let task = RegularTask(title: "do test", isComplete: true)
 		sut.addTasks(tasks: [task])
-		XCTAssertFalse(sut.completedTasks().isEmpty, "Completed task should be not empty")
+		XCTAssertFalse(sut.completedTasks().isEmpty, "sut.completedTasks не должен быть пустым")
 	}
 	
 	func test_completedTasks_withTaskStub_shouldBeNotEmpty() {
-		sut.addTasks(tasks: TaskStub.tasks)
-		XCTAssertFalse(!sut.completedTasks().isEmpty, "Completed tasks must be not empty")
+		let sut = makeSut()
+		sut.addTasks(tasks: [RegularTask(title: "test", isComplete: true)])
+		XCTAssertFalse(sut.completedTasks().isEmpty, "sut.completedTasks не должен быть пустым")
 	}
-	
+
 	// MARK: AddTasks
 	func test_addTasks_withNewTask_shouldBeNotEmpty() {
+		let sut = makeSut()
 		let task = RegularTask(title: "do test")
 		sut.addTasks(tasks: [task])
-		XCTAssertFalse(sut.allTasks().isEmpty, "Tasks must be not empty")
+		XCTAssertFalse(sut.allTasks().isEmpty, "sut.allTasks должен содержать задачи")
 	}
 	
 	func test_addTasks_withNewTask_comletedTasksShouldBeNotEmpty() {
+		let sut = makeSut()
 		let task = RegularTask(title: "do something", isComplete: true)
 		sut.addTasks(tasks: [task])
-		XCTAssertFalse(sut.completedTasks().isEmpty, "Completed tasks must be not empty")
+		XCTAssertFalse(sut.completedTasks().isEmpty, "sut.completedTasks должен содержать задачи")
 	}
 	
 	func test_addTasks_withNewTask_uncompletedTasksShouldBeNotEmpty() {
+		let sut = makeSut()
 		let task = RegularTask(title: "do homework", isComplete: false)
 		sut.addTasks(tasks: [task])
-		XCTAssertFalse(sut.uncompletedTasks().isEmpty, "Uncompleted tasks must be not empty")
+		XCTAssertFalse(sut.uncompletedTasks().isEmpty, "sut.uncompletedTasks должен содержать задачи")
 	}
 	
 	// MARK: Sorting
-	func test_sorted_withCustomTasks_firstTaskShouldBeImportantTask() {
+	func test_sorting_withCustomTasks_firstTaskShouldBeImportantTask() {
+		let sut = makeSut()
 		let regularTask = RegularTask(title: "do test")
 		let importantTask = ImportantTask(title: "do another", taskPriority: .high)
 		sut.addTasks(tasks: [regularTask, importantTask])
-		XCTAssertTrue(sut.allTasks().first === importantTask, "First task must be ImportantTask")
+		XCTAssertTrue(sut.allTasks().first === importantTask, "Первая задача дожна быть ImportantTask")
 	}
 	
-	func test_sorted_withCustomTasks_firstShouldBeTaskWithHighPriority() {
+	func test_sorting_withCustomTasks_firstShouldBeTaskWithHighPriority() {
+		let sut = makeSut()
 		let lowPriorityTask = ImportantTask(title: "do test", taskPriority: .low)
 		let highPriorityTask = ImportantTask(title: "do another", taskPriority: .high)
 		sut.addTasks(tasks: [lowPriorityTask, highPriorityTask])
-		XCTAssertTrue(sut.allTasks().first === highPriorityTask, "First Task should be ImportantTask with high priority")
+		XCTAssertTrue(sut.allTasks().first === highPriorityTask, "Первая задача должна быть ImportantTask с высоким приоритетом")
 	}
 	
-	func test_sorted_withCustomTasks_firstShouldBeReturnLastAddedTask() {
+	func test_sorting_withCustomTasks_firstShouldBeReturnLastAddedTask() {
+		let sut = makeSut()
 		let firstTask = RegularTask(title: "do test")
 		let secondTask = RegularTask(title: "do another test")
 		sut.addTasks(tasks: [firstTask, secondTask])
-		XCTAssertTrue(sut.uncompletedTasks().first === secondTask, "First task should be last added task")
+		XCTAssertTrue(sut.uncompletedTasks().first === secondTask, "Первая задача должна быть последняя добавленная")
 	}
 	
+	func test_sorting_with3Tasks_tasksShouldBeInRightPosition() {
+		let sut = makeSut()
+		let mediumPriorityTask = ImportantTask(title: "do test", taskPriority: .medium)
+		let highPriorityTask = ImportantTask(title: "do another", taskPriority: .high)
+		let regularTask = RegularTask(title: "do test")
+		let sortedTasks = [highPriorityTask, mediumPriorityTask, regularTask]
+		
+		sut.addTasks(tasks: [regularTask, highPriorityTask, mediumPriorityTask])
+		
+		XCTAssertEqual(sortedTasks, sut.allTasks(), "Задачи отсортированы неверно")
+	}
+}
+
+private extension OrderedTaskManagerTests {
+	func makeSut() -> OrderedTaskManager {
+		OrderedTaskManager(taskManager: TaskManager())
+	}
 }
--- Framework/TaskManagerPackage/Tests/TaskManagerPackageTests/TaskManagerTests.swift
+++ Framework/TaskManagerPackage/Tests/TaskManagerPackageTests/TaskManagerTests.swift
@@ -2,7 +2,7 @@
 //  TaskManagerTests.swift
 //  TodoListTests
 //
-//  Created by Владимир Свиридов on 12.03.2023.
+//  Created by Владимир Свиридов и Вадим Гамзаев on 12.03.2023.
 //
 
 import XCTest
@@ -10,119 +10,196 @@ import XCTest
 
 final class TaskManagerTests: XCTestCase {
 
-	var sut: TaskManager!
-
-	override func setUp() {
-		super.setUp()
-		sut = TaskManager()
-	}
-
-	override func tearDown() {
-		sut = nil
-		super.tearDown()
-	}
-
-	func test_allTasks_withTasksFromRepository_shouldBeNotEmpty() {
+	// MARK: - func allTasks()
+	func test_allTasks_withTasksFromStub_shouldBeNotEmpty() {
+		let sut = makeSut()
+		
 		sut.addTasks(tasks: TaskStub.tasks)
 
-		XCTAssertFalse(sut.allTasks().isEmpty, "Tasks must be not empty")
+		XCTAssertFalse(
+			sut.allTasks().isEmpty,
+			"Возвращаемый массив задач, тасков, не должен быть пустым."
+		)
 	}
 
 	func test_allTasks_initialState_shouldBeEmpty() {
-		XCTAssertTrue(sut.allTasks().isEmpty, "Tasks must be empty")
+		let sut = makeSut()
+		
+		XCTAssertTrue(
+			sut.allTasks().isEmpty,
+			"Инициализированный экземпляр класса TaskManager не должен содержать задач."
+		)
 	}
 
-	func test_allTasks_shouldGetArrayOfTaskType() {
-		XCTAssertTrue((sut.allTasks() as Any) is [Task], "Tasks have another type")
+	func test_allTasks_shouldReturnArrayOfTaskType() {
+		let sut = makeSut()
+		
+		XCTAssertTrue(
+			(sut.allTasks() as Any) is [Task],
+			"Обекты в возвращаемом массиве Tasks не соответсвуют типу Task.")
 	}
 
-	func test_completedTasks_withTasksFromRepository_shouldBeNotEmpty() {
+	// MARK: - func completedTasks()
+	func test_completedTasks_withTasksFromStub_shouldBeNotEmpty() {
+		let sut = makeSut()
+		
 		sut.addTasks(tasks: TaskStub.tasks)
 
-		XCTAssertFalse(sut.completedTasks().isEmpty, "Tasks must be not empty")
+		XCTAssertFalse(
+			sut.completedTasks().isEmpty,
+			"Возвращаемый массив завершенных задач не должен быть пустым."
+		)
 	}
 
 	func test_completedTasks_initialState_shouldBeEmpty() {
-		XCTAssertTrue(sut.completedTasks().isEmpty, "Tasks must be empty")
+		let sut = makeSut()
+		
+		XCTAssertTrue(
+			sut.completedTasks().isEmpty,
+			"Инициализированный экземпляр класса TaskManager не должен содержать завершенных задач."
+		)
 	}
 
-	func test_completedTasks_shouldGetArrayOfTaskType() {
-		XCTAssertTrue((sut.completedTasks() as Any) is [Task], "Tasks have another type")
+	func test_completedTasks_withTasksFromStub_shouldReturnArrayOfTaskType() {
+		let sut = makeSut()
+		
+		sut.addTasks(tasks: TaskStub.tasks)
+		
+		XCTAssertTrue(
+			(sut.completedTasks() as Any) is [Task],
+			"Обекты в возвращаемом массиве Tasks не соответсвуют типу Task."
+		)
 	}
 
-	func test_completedTasks_withTasksFromRepository_shouldHaveOnlyComletedTasks() {
+	func test_completedTasks_withTasksFromStub_shouldHaveOnlyComletedTasks() {
+		let sut = makeSut()
+		
+		sut.addTasks(tasks: TaskStub.tasks)
+		let uncompletedTasks = sut.completedTasks().filter { $0.isComplete == false }
+
+		XCTAssertTrue(
+			uncompletedTasks.isEmpty,
+			"Массив Tasks должен содержать только завершенные задачи."
+			)
+	}
+
+	// MARK: - func uncompletedTasks()
+	func test_uncompletedTasks_withTasksFromStub_shouldBeNotEmpty() {
+		let sut = makeSut()
+		
 		sut.addTasks(tasks: TaskStub.tasks)
 
-		XCTAssertTrue((sut.completedTasks().filter { $0.isComplete == false }.isEmpty ), "Tasks have uncompleted task")
-	}
-
-	func test_uncompletedTasks_withTasksFromRepository_shouldBeNotEmpty() {
-		sut.addTasks(tasks: TaskStub.tasks)
-
-		XCTAssertFalse(sut.uncompletedTasks().isEmpty, "Tasks must be not empty")
+		XCTAssertFalse(
+			sut.uncompletedTasks().isEmpty,
+			"Возвращаемый массив не завершенных задач не должен быть пустым."
+		)
 	}
 
 	func test_uncompletedTasks_initialState_shouldBeEmpty() {
-		XCTAssertTrue(sut.uncompletedTasks().isEmpty, "Tasks must be empty")
+		let sut = makeSut()
+		
+		XCTAssertTrue(
+			sut.uncompletedTasks().isEmpty,
+			"Инициализированный экземпляр класса TaskManager не должен содержать завершенных задач."
+			)
 	}
 
-	func test_uncompletedTasks_shouldGetArrayOfTaskType() {
-		XCTAssertTrue((sut.uncompletedTasks() as Any) is [Task], "Tasks have another type")
-	}
-
-	func test_uncompletedTasks_withTasksFromRepository_shouldHaveOnlyUncompletedTasks() {
+	func test_uncompletedTasks_withTasksFromStub_shouldReturnArrayOfTaskType() {
+		let sut = makeSut()
+		
 		sut.addTasks(tasks: TaskStub.tasks)
-
-		XCTAssertTrue((sut.uncompletedTasks().filter { $0.isComplete == true }.isEmpty ), "Tasks have completed task")
-	}
-
-	func testAddTaskShouldIReturnTaskCountPlusOneWhenAddOneTask() {
-		let taskCount = sut.allTasks().count
-		let task = TaskStub.singleTask
-		sut.addTask(task: task)
-
-		XCTAssert(
-			sut.allTasks().count == taskCount + 1,
-			"Test failed: Number of tasks must be increased by one when one task added"
+		
+		XCTAssertTrue(
+			(sut.uncompletedTasks() as Any) is [Task],
+			"Обекты в возвращаемом массиве Tasks не соответсвуют типу Task."
 		)
 	}
 
-	func testAddTaskShouldIReturnTrueWhenCallConteinWithAddedTask() {
-		let task = TaskStub.singleTask
+	func test_uncompletedTasks_withTasksFromStub_shouldHaveOnlyUncompletedTasks() {
+		let sut = makeSut()
+		
+		sut.addTasks(tasks: TaskStub.tasks)
+		let completedTasks = sut.uncompletedTasks().filter { $0.isComplete == true }
+
+		XCTAssertTrue(
+			completedTasks.isEmpty,
+			"Массив Tasks должен содержать только не завершенные задачи."
+		)
+	}
+
+	// MARK: - func addTask()
+
+	func test_addTask_withAddedNewSingleTask_ShouldIncreaseTheCounOfTasksByOne() {
+		let sut = makeSut()
+		
+		let expectedTaskCount = sut.allTasks().count + 1
+		let task = TaskStub.tasks[0]
 		sut.addTask(task: task)
 
-		XCTAssert(sut.allTasks().contains { $0 === task }, "TaskManager should contain added task")
+		XCTAssertEqual(
+			expectedTaskCount,
+			sut.allTasks().count,
+			"При добавлении новой задачи, возвращаемое количество задач должно увеличится на 1"
+		)
 	}
 
-	func testAddTasksShouldBeReturnCountOfAddedTasksWhenAddArrayOfTasks() {
-		let taskCount = TaskStub.tasks.count
+	func test_addTask_withAddedNewSingleTask_ShouldConteinAddedTask() {
+		let sut = makeSut()
+		let task = TaskStub.tasks[0]
+		
+		sut.addTask(task: task)
 
+		XCTAssertTrue(
+			sut.allTasks().contains { $0 === task },
+			"В возвращаемом массиве должна содержаться добавленная задача."
+		)
+	}
+
+	// MARK: - func addTasks()
+	func test_AddTasks_withTasksFromStub_ShouldBeReturnEqualTaskCount() {
+		let sut = makeSut()
+		
+		let expectedTaskCount = TaskStub.tasks.count
 		sut.addTasks(tasks: TaskStub.tasks)
 
-		XCTAssert(sut.allTasks().count == taskCount, "Test failed: Number of tasks must be equal added tasks array count")
+		XCTAssertEqual(
+			sut.allTasks().count,
+			expectedTaskCount,
+			"Количество возвращаемых задач должно соответствовать количеству добавленных"
+		)
 	}
 
-	func testRemoveTaskShouldBeReturn0CountWhenTaskRemoved() {
-		let task = TaskStub.singleTask
+	// MARK: - func removeTask()
+	func test_removeTask_withTaskFromStub_ShouldBeReturnZeroCount() {
+		let sut = makeSut()
+		let task = TaskStub.tasks[0]
+		sut.addTask(task: task)
+		
+		sut.removeTask(task: task)
+
+		XCTAssertEqual(
+			sut.allTasks().count,
+			Int.zero,
+			"После удаления задачи, количество возвращаемых задач должно ровняться нулю"
+		)
+	}
+
+	func test_RemoveTask_ShouldNotContainRemovedTask() {
+		let sut = makeSut()
+		let task = TaskStub.tasks[0]
 		sut.addTask(task: task)
 
 		sut.removeTask(task: task)
 
-		XCTAssert(
-			sut.allTasks().count == Int.zero,
-			"Test failed: Number of tasks must be 0"
+		XCTAssertFalse(
+			sut.allTasks().contains { $0 === task },
+			"Возвращаемый массив задач не должен содержать удаленную задачу"
 		)
 	}
+}
 
-	func testRemoveTaskShouldBeReturnFalseWhenCallConteinWithRemovedTask() {
-		let task = TaskStub.singleTask
-		sut.addTask(task: task)
-
-		sut.removeTask(task: task)
-
-		XCTAssert(
-			!sut.allTasks().contains { $0 === task },
-			"Test failed: TaskManager should not contain removed task"
-		)
+private extension TaskManagerTests {
+	func makeSut() -> TaskManager {
+		return TaskManager()
 	}
 }
