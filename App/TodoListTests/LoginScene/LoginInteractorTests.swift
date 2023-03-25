454adce01bcf6f2082da70a80bcbf19b72c714eb
Supodoco <76879150+Supodoco@users.noreply.github.com>
2023-03-25 16:01:40 +0300

    LoginInteractorTests added

---
+++ App/TodoListTests/LoginScene/LoginInteractorTests.swift
@@ -0,0 +1,34 @@
+//
+//  LoginInteractorTests.swift
+//  
+//
+//  Created by Supodoco on 25.03.2023.
+//
+
+import XCTest
+@testable import TodoList
+
+final class LoginInteractorTests: XCTestCase {
+	
+	// MARK: - Internal Properties
+	private let worker = LoginWorkerSpy()
+	private let presenter = LoginPresenterSpy()
+	
+	// MARK: - Internal Methods
+	func test_login_withValidRequest_shouldSuccessfullyLoggedIn() {
+		let sut = makeSut()
+		let request = LoginModels.Request(login: "validEmail", password: "validPass")
+		
+		sut.login(request: request)
+		
+		XCTAssertTrue(worker.isCalledLogin, "Не вызван worker.login(:)")
+		XCTAssertTrue(presenter.isCalledPresent, "Не вызван  presenter.present(:)")
+	}
+}
+
+// MARK: - Making sut
+private extension LoginInteractorTests {
+	func makeSut() -> LoginInteractor {
+		LoginInteractor(worker: worker, presenter: presenter)
+	}
+}
---
+++ App/TodoListTests/LoginScene/TestDoubles/LoginWorkerSpy.swift
@@ -0,0 +1,20 @@
+//
+//  LoginWorkerSpy.swift
+//  TodoListTests
+//
+//  Created by Supodoco on 25.03.2023.
+//
+
+@testable import TodoList
+
+class LoginWorkerSpy: ILoginWorker {
+	
+	// MARK: - Internal Properties
+	private(set) var isCalledLogin = false
+		
+	// MARK: - Internal Methods
+	func login(login: String, password: String) -> Bool {
+		isCalledLogin = true
+		return true
+	}
+}
